c6a0f8d0d99b9af34f3027ca6418efc53bbd1385
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-09-18 12:44:08 -0700

    Completed.

---
+++ src/components/Board.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Square from "./Square";
+
+const Board = ({ squares, onClick }) => (
+  <div className="board">
+    {squares.map((square, index) => (
+      <Square key={index} onClick={() => onClick(index)} value={square} />
+    ))}
+  </div>
+);
+
+export default Board;
